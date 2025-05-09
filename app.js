3b6ab25a1f76749727cb66c5e5f979def6b20f8a
Muhammad Mehdi <muhammadmehdi0555@gmail.com>
2025-05-09 21:43:23 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,395 @@
+
+
+var mobile = {
+    vivo: {
+      X50_Pro: {
+        img: 'mobail/vivox50pro.png',
+        modelName: 'Vivo X50 Pro',
+        displaySize: '6.56 inches',
+        ram: '8GB',
+        storage: '256GB',
+        camera: 'Quad 48MP + 13MP ',
+        price: "$599.99"
+      },
+      V21e: {
+        img: 'mobail/v21e.jpg',
+        modelName: 'Vivo V21e',
+        displaySize: '6.44 inches',
+        ram: '8GB',
+        storage: '128GB',
+        camera: 'Dual 64MP + 8MP',
+        price: "$399.99"
+  
+      },
+      Y73s: {
+        img: 'mobail/y73s.jfif',
+        modelName: 'Vivo Y73s',
+        displaySize: '6.44 inches',
+        ram: '8GB',
+        storage: '128GB',
+        camera: 'Triple 48MP + 8MP',
+        price: "$329.99"
+      },
+      S1_Pro: {
+        img: 'mobail/s1pro.webp',
+        modelName: 'Vivo S1 Pro',
+        displaySize: '6.38 inches',
+        ram: '8GB',
+        storage: '128GB',
+        camera: 'Quad 48MP + 8MP',
+        price: "$349.99"
+      },
+      Y20: {
+        img: 'mobail/y20.jfif',
+        modelName: 'Vivo Y20',
+        displaySize: '6.51 inches',
+        ram: '4GB',
+        storage: '64GB',
+        camera: 'Triple 13MP + 2MP',
+        price: "$199.99"
+      },
+      Z1x: {
+        img: 'mobail/z1x.jpg',
+        modelName: 'Vivo Z1x',
+        displaySize: '6.38 inches',
+        ram: '6GB',
+        storage: '128GB',
+        camera: 'Triple 48MP + 8MP',
+        price: "$289.99"
+      }
+    },
+    oppo: {
+      A9: {
+        img: 'mobail/a9.jpg',
+        modelName: "Oppo A9",
+        displaySize: "6.5 inches",
+        ram: "6 GB",
+        storage: "128 GB",
+        camera: "48 MP + 8 MP",
+        price: "$250"
+      },
+      Reno_3_Pro: {
+        img: 'mobail/reno3.jfif' ,
+        modelName: "Oppo Reno 3 Pro",
+        displaySize: "6.4 inches",
+        ram: "8 GB",
+        storage: "128 GB",
+        camera: "64 MP + 13 MP ",
+        price: "$400"
+      },
+      Find_X2_Pro: {
+        img: 'mobail/x2pro.jpg',
+        modelName: "Oppo Find X2 Pro",
+        displaySize: "6.7 inches",
+        ram: "12 GB",
+        storage: "512 GB",
+        camera: "48 MP + 13 MP",
+        price: "$1100"
+      },
+      F15: {
+        img: 'mobail/15.jpg',
+        modelName: "Oppo F15",
+        displaySize: "6.4 inches",
+        ram: "8 GB",
+        storage: "128 GB",
+        camera: "48 MP + 8 MP",
+        price: "$300"
+      },
+      A52: {
+        img: 'mobail/a52.jfif',
+        modelName: "Oppo A52",
+        displaySize: "6.5 inches",
+        ram: "4 GB",
+        storage: "128 GB",
+        camera: "12 MP + 8 MP",
+        price: "$200"
+      },
+      Reno_4_Pro: {
+        img: 'mobail/4pro.jpg',
+        modelName: "Oppo Reno 4 Pro",
+        displaySize: "6.5 inches",
+        ram: "8 GB",
+        storage: "256 GB",
+        camera: "48 MP + 8 MP",
+        price: "$550"
+      }
+    },
+    samsung: {
+      Galaxy_S21: {
+        img: 'mobail/s21.png',
+        modelName: "Samsung Galaxy S21",
+        displaySize: "6.2 inches",
+        ram: "8 GB",
+        storage: "128 GB",
+        camera: "12 MP + 64 MP",
+        price: "$799.99"
+      },
+      Galaxy_S21_Ultra: {
+        img: 'mobail/s21ultra.webp',
+        modelName: "Samsung Galaxy S21 Ultra",
+        displaySize: "6.8 inches",
+        ram: "12 GB",
+        storage: "256 GB",
+        camera: "108 MP + 12 MP",
+        price: "$1199.99"
+      },
+      Galaxy_A32: {
+        img: 'mobail/a32.png',
+        modelName: "Samsung Galaxy A32",
+        displaySize: "6.4 inches",
+        ram: "6 GB",
+        storage: "128 GB",
+        camera: "64 MP + 8 MP",
+        price: "$279.99"
+      },
+      Galaxy_A52: {
+        img: 'mobail/a52.webp',
+        modelName: "Samsung Galaxy A52",
+        displaySize: "6.5 inches",
+        ram: "6 GB",
+        storage: "128 GB",
+        camera: "64 MP + 12 MP",
+        price: "$349.99"
+      },
+      Galaxy_Note_20_Ultra: {
+        img: 'mobail/20ultra.webp',
+        modelName: "Samsung Galaxy Note 20 Ultra",
+        displaySize: "6.9 inches",
+        ram: "12 GB",
+        storage: "256 GB",
+        camera: "108 MP + 12 MP",
+        price: "$1299.99"
+      },
+      Galaxy_Z_Fold_3: {
+        img: 'mobail/zfold3.png',
+        modelName: "Samsung Galaxy Z Fold 3",
+        displaySize: "7.6 inches (Main), 6.2 inches (Cover)",
+        ram: "12 GB",
+        storage: "256 GB",
+        camera: "12 MP (Main)",
+        price: "$1,799.99",
+      },
+      Galaxy_Note_20: {
+        img: 'mobail/note20.webp',
+        modelName: "Samsung Galaxy Note 20",
+        displaySize: "6.7 inches",
+        ram: "8 GB",
+        storage: "256 GB",
+        camera: "12 MP (Main), 12 MP",
+        price: "$999.99"
+      },
+  
+      Galaxy_S20_FE: {
+        img: 'mobail/s20fer.png',
+        modelName: 'Samsung Galaxy S20 FE',
+        displaySize: '6.5 inches',
+        ram: '6GB',
+        storage: '128GB',
+        camera: '12MP + 8MP + 12MP',
+        price: '$599.99'
+  
+      }
+    },
+    infinix: {
+      Note_10_Pro: {
+        img: 'mobail/10pro.png',
+        modelName: 'Infinix Note 10 Pro',
+        displaySize: '6.95 inches',
+        ram: '8 GB',
+        storage: '128 GB',
+        camera: '64 MP + 8 MP',
+        price: "$250"
+      },
+      Zero_X_Pro: {
+        img: 'mobail/xeroxpro.webp',
+        modelName: 'Infinix Zero X Pro',
+        displaySize: '6.67 inches',
+        ram: '8 GB',
+        storage: '256 GB',
+        camera: '108 MP + 8 MP',
+        price: "$300"
+      },
+      Hot_10: {
+        img: 'mobail/hot10.png',
+        modelName: 'Infinix Hot 10',
+        displaySize: '6.78 inches',
+        ram: '6 GB',
+        storage: '128 GB',
+        camera: '16 MP + 2 MP',
+        price: "$150"
+      },
+      Smart_5A: {
+        img: 'mobail/smart54a.png',
+        modelName: 'Infinix Smart 5A',
+        displaySize: '6.52 inches',
+        ram: '2 GB',
+        storage: '32 GB',
+        camera: '8 MP + AI Lens',
+        price: "$100"
+      },
+      Hot_10T: {
+        img: 'mobail/25.webp',
+        modelName: 'Infinix Hot 10T',
+        displaySize: '6.82 inches',
+        ram: '4 GB',
+        storage: '64 GB',
+        camera: '48 MP + 2 MP',
+        price: "$130"
+      },
+      S5_Pro: {
+        img: 'mobail/s5pro.webp',
+        modelName: 'Infinix S5 Pro',
+        displaySize: '6.53 inches',
+        ram: '6 GB',
+        storage: '128 GB',
+        camera: '48 MP + 2 MP',
+        price: "$180"
+      }
+    },
+    google: {
+      Pixel_6: {
+        img: 'mobail/6.webp',
+        modelName: 'Google Pixel 6',
+        displaySize: '6.4 inches',
+        ram: '8 GB',
+        storage: '128 GB',
+        camera: 'Dual 50 MP',
+        price: '$699'
+      },
+      Pixel_6_Pro: {
+        img: 'mobail/2.webp',
+        modelName: 'Google Pixel 6 Pro',
+        displaySize: '6.7 inches',
+        ram: '12 GB',
+        storage: '256 GB',
+        camera: 'Triple 50 MP',
+        price: '$899'
+      },
+      Pixel_5a: {
+        img: 'mobail/5a.jpg',
+        modelName: 'Google Pixel 5a',
+        displaySize: '6.34 inches',
+        ram: '6 GB',
+        storage: '128 GB',
+        camera: 'Dual 12.2 MP',
+        price: '$449'
+      },
+      Pixel_5: {
+        img: 'mobail/5as.jpg',
+        modelName: 'Google Pixel 5',
+        displaySize: '6.0 inches',
+        ram: '8 GB',
+        storage: '128 GB',
+        camera: 'Dual 12.2 MP',
+        price: '$599'
+      },
+      Pixel_4a_5G: {
+        img: 'mobail/4a5g.jpg',
+        modelName: 'Google Pixel 4a 5G',
+        displaySize: '6.2 inches',
+        ram: '6 GB',
+        storage: '128 GB',
+        camera: 'Dual 12.2 MP',
+        price: '$499'
+      },
+      Pixel_4a: {
+        img: 'mobail/564asd.jfif',
+        modelName: 'Google Pixel 4a',
+        displaySize: '5.81 inches',
+        ram: '6 GB',
+        storage: '128 GB',
+        camera: '12.2 MP',
+        price: '$349'
+      }
+    },
+  
+  }
+
+var searchEn=document.getElementById('searchEn')
+var main = document.getElementById('main')
+
+
+
+for (let brand in mobile){
+
+    for (var model in mobile[brand]){
+
+      main.innerHTML+=
+      `<div class="box">
+      <div class="box1">
+
+      <img src="${mobile[brand][model].img}">
+      
+           <div class="card-body">
+        <h5 class="card-title">${mobile[brand][model].modelName}</h5>
+        <h6 class="card-subtitle mb-2 text-body-secondary"> <span class="color-change"> RAM: </span> ${mobile[brand][model].ram}</h6>
+        <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Storage: </span> ${mobile[brand][model].storage}</h6>
+        <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Display: </span> ${mobile[brand][model].displaySize}</h6>
+        <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Camera: </span> ${mobile[brand][model].camera}</h6>
+        <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Price: </span> ${mobile[brand][model].price}</h6>
+        
+      </div>
+      </div>
+      </div>
+      </div>
+      `
+
+
+
+
+
+
+    }
+
+}
+      function search() {
+  const query = searchEn.value.toLowerCase();
+  main.innerHTML = '';
+
+  for (let brand in mobile) {
+    for (let model in mobile[brand]) {
+      const phone = mobile[brand][model];
+      if (phone.modelName.toLowerCase().includes(query)) {
+        main.innerHTML += `
+        <div class="box">
+          <div class="box1">
+            <img src="${phone.img}">
+            <div class="card-body">
+              <h5 class="card-title">${phone.modelName}</h5>
+              <h6 class="card-subtitle mb-2 text-body-secondary"> <span class="color-change"> RAM: </span> ${phone.ram}</h6>
+              <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Storage: </span> ${phone.storage}</h6>
+              <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Display: </span> ${phone.displaySize}</h6>
+              <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Camera: </span> ${phone.camera}</h6>
+              <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Price: </span> ${phone.price}</h6>
+            </div>
+          </div>
+        </div>`;
+      }
+    }
+  }
+}
+
+
+function showAll() {
+  main.innerHTML = ''; 
+
+  for (let brand in mobile) {
+    for (let model in mobile[brand]) {
+      const phone = mobile[brand][model];
+      main.innerHTML += `
+      <div class="box">
+        <div class="box1">
+          <img src="${phone.img}">
+          <div class="card-body">
+            <h5 class="card-title">${phone.modelName}</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary"> <span class="color-change"> RAM: </span> ${phone.ram}</h6>
+            <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Storage: </span> ${phone.storage}</h6>
+            <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Display: </span> ${phone.displaySize}</h6>
+            <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Camera: </span> ${phone.camera}</h6>
+            <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Price: </span> ${phone.price}</h6>
+          </div>
+        </div>
+      </div>`;
+    }
+  }
+}
